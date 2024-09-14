0bff8a8420cf1b6282ff232c9f86dd821c76fb5a
LevaInformatik <111689867+LevaInformatik@users.noreply.github.com>
2024-09-14 17:20:53 +0300

    1

---
+++ задание2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+
+class Person
+{
+    private string Name;
+    private int Age;
+
+    public Person(string Name, int Age)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+
+    public string GetPersonInfo()
+    {
+        return $"Имя: {Name}, Возраст: {Age}";
+    }
+
+    public bool IsAdult()
+    {
+        if (this.Age >= 18)
+            return true;
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+
+        int age;
+        while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
+        {
+            Console.Write("Введите ваш возраст: ");
+        }
+
+        Person person = new Person(name, age);
+
+        Console.WriteLine(person.GetPersonInfo());
+        Console.WriteLine((person.IsAdult() ? "Взрослый" : "Не взрослый"));
+    }
+}
